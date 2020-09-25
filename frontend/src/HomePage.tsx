1f05db0a69fb8cb70c489e8d7872494514977c6f
Justin Grimes <jgrimes@relias.com>
2020-09-25 09:10:41 -0400

    Initial commit

---
+++ frontend/src/HomePage.tsx
@@ -0,0 +1,65 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { QuestionList } from './QuestionList';
+import { getUnansweredQuestions, QuestionData } from './QuestionData';
+import { PrimaryButton } from './Styles';
+import { Page } from './Page';
+import { PageTitle } from './PageTitle';
+
+import { useEffect, useState, FC } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+
+export const HomePage: FC<RouteComponentProps> = ({ history }) => {
+  const [questions, setQuestions] = useState<QuestionData[] | null>(null);
+  const [questionsLoading, setQuestionsLoading] = useState(true);
+
+  useEffect(() => {
+    const doGetUnansweredQuestions = async () => {
+      const unansweredQuestions = await getUnansweredQuestions();
+      setQuestions(unansweredQuestions);
+      setQuestionsLoading(false);
+    };
+    doGetUnansweredQuestions();
+  }, []);
+
+  const handleAskQuestionClick = () => {
+    history.push('/ask');
+  };
+
+  return (
+    <Page>
+      <div
+        css={css`
+          margin: 50px auto 20px auto;
+          padding: 30px 20px;
+          max-width: 600px;
+        `}
+      >
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+          `}
+        >
+          <PageTitle>Unanswered Questions</PageTitle>
+          <PrimaryButton onClick={handleAskQuestionClick}>
+            Ask a question
+          </PrimaryButton>
+        </div>
+        {questionsLoading ? (
+          <div
+            css={css`
+              font-size: 16px;
+              font-style: italic;
+            `}
+          >
+            Loading...
+          </div>
+        ) : (
+          <QuestionList data={questions || []} />
+        )}
+      </div>
+    </Page>
+  );
+};
---
+++ frontend/src/Question.tsx
@@ -0,0 +1,52 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { QuestionData } from './QuestionData';
+import { gray2, gray3 } from './Styles';
+import { Link } from 'react-router-dom';
+
+interface Props {
+  data: QuestionData;
+  showContent?: boolean;
+}
+
+export const Question: React.FC<Props> = ({ data, showContent = true }) => (
+  <div
+    css={css`
+      padding: 10px 0px;
+    `}
+  >
+    <Link
+      css={css`
+        text-decoration: none;
+        color: ${gray2};
+      `}
+      to={`questions/${data.questionId}`}
+    >
+      {data.title}
+    </Link>
+    {showContent && (
+      <div
+        css={css`
+          padding-bottom: 10px;
+          font-size: 15px;
+          color: ${gray2};
+        `}
+      >
+        {data.content.length > 50
+          ? `${data.content.substring(0, 50)}...`
+          : data.content}
+      </div>
+    )}
+    <div
+      css={css`
+        font-size: 12px;
+        font-style: italic;
+        color: ${gray3};
+      `}
+    >
+      {`Asked by ${
+        data.userName
+      } on ${data.created.toLocaleDateString()} ${data.created.toLocaleTimeString()}`}
+    </div>
+  </div>
+);
